67dd15c17efa8a9cc4ca88d27d04c5334a444255
Melvin Zaldana <melvinzaldana91@gmail.com>
2021-07-01 09:16:33 -0500

    Primer version de libreria de operaciones matematicas basicas

---
+++ index.js
@@ -0,0 +1,49 @@
+module.exports = {
+    /**
+     * Suma de 2 números
+     * @example
+     * n1 = 1, n2 = 3 => resultado 4
+     * @param {*} n1 Número 1 de la suma
+     * @param {*} n2 Número 2 de la suma
+     * @returns 
+     */
+    suma: function (n1,n2){
+        return n1+n2;
+    },
+    
+    /**
+     * Resta de 2 números
+     * @example
+     * n1 = 3, n2 = 1 => resultado 2
+     * @param {*} n1 Número 1 de la resta
+     * @param {*} n2 Número 2 de la resta
+     * @returns 
+     */
+    resta: function (n1,n2){
+        return n1-n2;
+    },
+
+    /**
+     * Multiplicación de 2 números
+     * @example
+     * n1 = 1, n2 = 3 => resultado 3
+     * @param {*} n1 Número 1 de la multiplicación
+     * @param {*} n2 Número 2 de la multiplicación
+     * @returns 
+     */
+    multiplicacion: function (n1,n2){
+        return n1*n2;
+    },
+
+    /**
+     * División de 2 números
+     * @example
+     * n1 = 9, n2 = 3 => resultado 3
+     * @param {*} n1 Número 1 de la división
+     * @param {*} n2 Número 2 de la división
+     * @returns 
+     */
+    dividir: function (n1,n2){
+        return n1/n2;
+    },
+}
---
+++ pruebas.js
@@ -0,0 +1,6 @@
+const mz = require('.')
+
+console.log(mz.suma(34,12));
+console.log(mz.resta(34,12));
+console.log(mz.multiplicacion(34,12));
+console.log(mz.dividir(34,12));
